d14ff154537083ac6d576fb5b892ffe46df484a9
Hatvis7 <shamil.gasanov.2004@bk.ru>
2023-12-12 16:12:41 +0300

    add: visual updates
    * animations and UI updates
    * and other some fixes

--- src/source/js/script.js
+++ src/source/js/script.js
@@ -1,31 +1,33 @@
 
-// Product by @Hatvis7
+// Product by Monvis
 
-//****************************************************************************************************************/
+//*********************************************************************
 
-let message    = document.querySelector('.todo__input');
-let addBtn     = document.querySelector('.todo__btn');
+let message    = document.querySelector('.todo__input')
+let addBtn     = document.querySelector('.todo__btn')
 let inputField = document.querySelector('.todo__input-field')
-let plugText   = document.querySelector('.todo__list-plug');
-let todo       = document.querySelector('.todo__list');
-let checkbox   = document.querySelector('.input-checkbox');
-let deleteList = document.querySelector('.todo__delete-btn');
-let tracker    = document.querySelector('.todo__tracker');
+let plugText   = document.querySelector('.todo__list-plug')
+let todo       = document.querySelector('.todo__list')
+let deleteList = document.querySelector('.todo__delete-btn')
+let tracker    = document.querySelector('.todo__tracker')
+let taskInput  = document.querySelector('input[type="checkbox"]')
+let warning    = document.createElement('p')
 
-let warning    = document.createElement('p');
-
-//****************************************************************************************************************/
+//*********************************************************************
 
 // Event handlers
 
-addBtn.addEventListener('click', createTask);
+addBtn.addEventListener('click', createTask)
+message.addEventListener('keydown', EnterBtn)
+todo.addEventListener('click', deleteTask)
+todo.addEventListener('click', checkInput)
+todo.addEventListener('click', isTaskCompleate)
 
-todo.addEventListener('click', deleteTask);
 
-
-let todoList = [];
+let todoList = []
 
 // General App functions
+
 function createTask() {
     
     if ( message.value != '' ) {
@@ -35,102 +37,119 @@ function createTask() {
             checked: false
         }
 
-        todoList.push(newTodo);
-        displayMessages();
+        todoList.push(newTodo)
+        displayMessages()
 
-        addBtn.style.opacity = '0.5';
-        plugText.style = 'display: none';
-        
+        addBtn.style.opacity = '0.5'
+        plugText.style = 'display: none'
+
     } else {
 
-        emptyInputField();
-        warning.style.removeProperty('animation');
-    };
-};
+        emptyInputField()
+        warning.style.removeProperty('animation')
+    }
+}
 
 function displayMessages() {
 
-    let displayMessage = '';
+    let displayMessage = ''
 
     todoList.forEach((elem, i) => {
 
         displayMessage += `
 
             <li class="todo__item" id="${i}">
-                <input type="checkbox" class="input-checkbox" id="item__${i}"></input>
-                <label for="item__${i}" id="item__${i}"><p>${elem.text}</p><span data-action="delete"><img data-action="delete" src="img/bin.svg" alt=""></span></label>
+                <label id="item__${i}">
+                    <input type="checkbox" class="input-checkbox" id="item__${i}"></input>
+                    <p class="text">${elem.text}</p><span data-action="delete"><img data-action="delete" src="img/bin.svg" alt=""></span>
+                </label>
             </li>
-        `;
+        `
 
-        todo.innerHTML = displayMessage;
-        message.value = '';
-        taskTracker();
-    });
-};
+        todo.innerHTML = displayMessage
+        message.value = ''
+        taskTracker()
+    })
+}
 
 function taskTracker() {
 
-    counter = todoList.length;
-    innerText = `You have ${counter} active task(s)`;
-    tracker.innerHTML = innerText;
-};
+    counter = todoList.length
+    tracker.innerHTML = `You have ${counter} active task(s)`
+}
+
+function isTaskCompleate(elem) {
+
+    checkbox = elem.target.closest('.todo__item')
+    checkbox.classList.toggle('done')
+}
 
 function deleteTask(elem) {
 
-    if (elem.target.dataset.action !== 'delete') return;
+    if (elem.target.dataset.action !== 'delete') return
 
-    const item = elem.target.closest('.todo__item');
-    const id = Number(item.id);
+    const item = elem.target.closest('.todo__item')
+    const id = Number(item.id)
 
-    const index = todoList.findIndex((task) => task.id === id);
+    const index = todoList.findIndex((task) => task.id === id)
 
-    todoList.splice(index, 1);
-    item.remove();
-    taskTracker();
+    item.style.animation = '0.5s fade-out forwards'
+    setTimeout(() => {
 
-    if ( todoList.length == 0 ) { plugText.style = 'display: block' };
-};
+        todoList.splice(index, 1)
+        item.remove()
+        taskTracker()
+
+        if ( todoList.length == 0 ) { plugText.style = 'display: block' }
+
+    }, 600)
+
+    if ( todoList.length == 0 ) { plugText.style = 'display: block' }
+}
 
 function emptyInputField() {
 
-    warning.classList.add('warning');
-    warning.innerHTML = ('This field cannot be empty');
-    inputField.appendChild(warning);
+    warning.classList.add('warning')
+    warning.innerHTML = ('This field cannot be empty')
+    inputField.appendChild(warning)
 
-    warning.style.removeProperty('animation');
+    warning.style.removeProperty('animation')
 
-    addBtn.style.opacity = '0.5';
-    message.style.border = '1px solid #ed3511';
-};
+    addBtn.style.opacity = '0.5'
+    message.style.border = '1px solid #ed3511'
+}
 
 // Is Inpit Field Empty?
 message.oninput = function() {
 
     if ( message.value == '' ) {
 
-        emptyInputField();
+        emptyInputField()
 
     } else {
 
-        addBtn.style.opacity = '1';
-        message.style.border = '1px solid rgba(0, 0, 0, 0.075)';
-        warning.style.animation = '1s dysplayNone forwards';
-    };
-};
+        addBtn.style.opacity = '1'
+        message.style.border = '1px solid rgba(0, 0, 0, 0.075)'
+        warning.style.animation = '1s fade-out forwards'
+    }
+}
 
 // Adding a task with "Enter" button
-message.addEventListener('keydown', function(e) {
+function EnterBtn(e) { if ( e.keyCode === 13 ) { createTask() } }
 
-    if ( e.keyCode === 13 ) {
-
-        createTask();
-    };
-});
 
 deleteList.addEventListener('click', function() {
 
-    todo.innerHTML = '';
-    todoList.length = 0; 
-    taskTracker();
-    plugText.style = 'display: block';
-});
+    todo.style.animation = '1s fade-out forwards'
+
+    setTimeout(() => {
+
+        todo.innerHTML = ''
+        todoList.length = 0
+        taskTracker()
+        plugText.style = 'display: block'
+
+    }, 600)
+
+    todo.removeAttribute('style')
+})
