4f78fcb827469cbe3a0066e1e634759a3bcf9ae7
Shamil Gasanov <74822512+Monvis@users.noreply.github.com>
2025-01-19 19:23:07 +0300

    build: transition from gulp to vite
    * build: сhanged assembly and file structure
    * add: сlicking on a checkbox tracks its status and transfers the state to the corresponding element in the task array
    * add: new page logo
    * fix: fixed a bug with incorrect deletion of tasks from the task array and incorrect display of the list on the page in 'isTaskCompleate' function

--- src/js/script.js
+++ src/js/script.js
@@ -1,39 +1,37 @@
 
-// Product by Monvis
+// Product and supported by Monvis
+
+import '../scss/style.scss'
+import bin from '../assets/bin.svg'
 
 //*********************************************************************
-
-let message    = document.querySelector('.todo__input')
-let addBtn     = document.querySelector('.todo__btn')
-let inputField = document.querySelector('.todo__input-field')
-let plugText   = document.querySelector('.todo__list-plug')
-let todo       = document.querySelector('.todo__list')
-let deleteList = document.querySelector('.todo__delete-btn')
-let tracker    = document.querySelector('.todo__tracker')
-let taskInput  = document.querySelector('input[type="checkbox"]')
-let warning    = document.createElement('p')
-
+const message    = document.querySelector('.todo__input')
+const addBtn     = document.querySelector('.todo__btn')
+const inputField = document.querySelector('.todo__input-field')
+const plugText   = document.querySelector('.todo__list-plug')
+const todo       = document.querySelector('.todo__list')
+const deleteList = document.querySelector('.todo__delete-btn')
+const tracker    = document.querySelector('.todo__tracker')
+const warning    = document.createElement('p')
 //*********************************************************************
 
 // Event handlers
 
 addBtn.addEventListener('click', createTask)
-message.addEventListener('keydown', EnterBtn)
 todo.addEventListener('click', deleteTask)
 todo.addEventListener('click', isTaskCompleate)
+message.addEventListener('keydown', enterBtn)
 
-
+// All tasks here...
 let todoList = []
 
 // General App functions
-
 function createTask() {
-    
     if ( message.value != '' ) {
-
         let newTodo = {
             text: message.value,
-            checked: false
+            checked: false,
+            id: todoList.length
         }
 
         todoList.push(newTodo)
@@ -43,24 +41,21 @@ function createTask() {
         plugText.style = 'display: none'
 
     } else {
-
         emptyInputField()
         warning.style.removeProperty('animation')
     }
 }
 
 function displayMessages() {
-
     let displayMessage = ''
-
     todoList.forEach((elem, i) => {
 
-        displayMessage += `
-
+        displayMessage += 
+        `
             <li class="todo__item" id="${i}">
                 <label id="item__${i}">
-                    <input type="checkbox" class="input-checkbox" id="item__${i}"></input>
-                    <p class="text">${elem.text}</p><span data-action="delete"><img data-action="delete" src="img/bin.svg" alt=""></span>
+                    <input type="checkbox" class="input-checkbox" id="${i}"></input>
+                    <p class="text">${elem.text}</p><span data-action="delete"><img data-action="delete" src=${bin} alt="delete"></span>
                 </label>
             </li>
         `
@@ -72,42 +67,41 @@ function displayMessages() {
 }
 
 function taskTracker() {
-
-    counter = todoList.length
+    let counter = todoList.length
     tracker.innerHTML = `You have ${counter} active task(s)`
 }
 
 function isTaskCompleate(elem) {
+    if (elem.target.type !== 'checkbox') { return 0 }
 
+    let checkbox
     checkbox = elem.target.closest('.todo__item')
-    checkbox.classList.toggle('done')
+    checkbox.classList.toggle('checked')
+
+    // Toggle checked status (true:false) for current element in todo array
+    for (let i = 0; i < todoList.length; i++) {
+        if (todoList[i].id == checkbox.id) {
+            todoList[i].checked === false ? todoList[i].checked = true : todoList[i].checked = false
+        }
+    }
 }
 
 function deleteTask(elem) {
-
-    if (elem.target.dataset.action !== 'delete') return
+    if (elem.target.dataset.action !== 'delete') return 0
 
     const item = elem.target.closest('.todo__item')
     const id = Number(item.id)
-
     const index = todoList.findIndex((task) => task.id === id)
 
     item.style.animation = '0.5s fade-out forwards'
-    setTimeout(() => {
-
-        todoList.splice(index, 1)
-        item.remove()
-        taskTracker()
-
-        if ( todoList.length == 0 ) { plugText.style = 'display: block' }
-
-    }, 600)
+    todoList.splice(index, 1)
+    item.remove()
+    taskTracker()
 
     if ( todoList.length == 0 ) { plugText.style = 'display: block' }
 }
 
 function emptyInputField() {
-
     warning.classList.add('warning')
     warning.innerHTML = ('This field cannot be empty')
     inputField.appendChild(warning)
@@ -122,11 +116,8 @@ function emptyInputField() {
 message.oninput = function() {
 
     if ( message.value == '' ) {
-
         emptyInputField()
-
     } else {
-
         addBtn.style.opacity = '1'
         message.style.border = '1px solid rgba(0, 0, 0, 0.075)'
         warning.style.animation = '1s fade-out forwards'
@@ -134,21 +125,18 @@ message.oninput = function() {
 }
 
 // Adding a task with "Enter" button
-function EnterBtn(e) { if ( e.keyCode === 13 ) { createTask() } }
+function enterBtn(e) { if ( e.keyCode === 13 ) { createTask() } }
 
 
 deleteList.addEventListener('click', function() {
-
     todo.style.animation = '1s fade-out forwards'
 
     setTimeout(() => {
-
         todo.innerHTML = ''
         todoList.length = 0
         taskTracker()
         plugText.style = 'display: block'
-
-    }, 600)
+    }, 500)
 
     todo.removeAttribute('style')
 })
